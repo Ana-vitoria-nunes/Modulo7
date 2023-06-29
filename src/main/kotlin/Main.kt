8d18b3f422d100e2f7c28428a22b1c279d1da8ba
Ana.Vitoria.Nunes <ana.nunes@zup.com.br>
2023-06-29 16:17:42 -0300

    teste

--- src/main/kotlin/Main.kt
+++
@@ -1,8 +0,0 @@
-fun main(args: Array<String>) {
-
-    println("Digite um número:")
-    var resposta= readln().toInt()
-    println("Número digitado: "+resposta)
-    resposta-=1
-    println("Número antecessor: "+resposta)
-}
